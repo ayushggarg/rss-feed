44653c1a772e52821dee0a5f6d020e0a87d2971d
ayushggarg <ayushggarg@gmail.com>
2016-06-17 12:34:08 +0530

    single url feed preview

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+$(function() {
+    $("a").linkpreview({
+        previewContainer: "#preview-container"
+    });
+    $("a").linkpreview({
+        previewContainer: "#preview-container2",
+        refreshButton: "#refresh-button",
+        previewContainerClass: "row-fluid",
+        errorMessage: "Invalid URL",
+        preProcess: function() {
+            console.log("preProcess");
+        },
+        onSuccess: function() {
+            console.log("onSuccess");
+        },
+        onError: function() {
+            console.log("onError");
+        },
+        onComplete: function() {
+            console.log("onComplete");
+        }
+    });
+    $("input").linkpreview({
+        previewContainer: "#preview-container2",
+        refreshButton: "#refresh-button",
+        previewContainerClass: "row-fluid",
+        errorMessage: "Invalid URL",
+        preProcess: function() {
+            console.log("preProcess");
+        },
+        onSuccess: function() {
+            console.log("onSuccess");
+        },
+        onError: function() {
+            console.log("onError");
+        },
+        onComplete: function() {
+            console.log("onComplete");
+        }
+    });
+
+});
---
+++ try.php
@@ -0,0 +1,31 @@
+ <?
+
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-linkpreview.css">
+</head>
+<body>
+    <div id="preview-container"></div>
+    <div class="row-fluid input-append">
+        <a href="#" id="linkurl"></a>
+    </div>
+    <div id="preview-container2"></div>
+    <script>
+        linkurl = "http://home.cern/scientists/updates/2016/06/track-latest-news-and-results-aida-2020"
+        document.getElementById("linkurl").href = linkurl;
+    </script>
+    <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+    <script type="text/javascript" src="js/proxy-ajax.js"></script>
+    <script type="text/javascript" src="js/bootstrap-linkpreview.js"></script>
+    <script type="text/javascript" src="js/main.js"></script> 
+</body>
+</html>
+ 
